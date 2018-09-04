6851736810cf34ba9e2cb9ccbad55f2a2f6a9bf2
Sendy Akbar <msendyakbars@gmail.com>
2018-09-05 00:01:36 +0700

    edit pola if

--- exercises4.js
+++ exercises4.js
@@ -1,27 +1,23 @@
 var nama = 'Sendy'
-var peran = 'Ksatria'
+var peran = 'tabib'
 
-if (nama === '' && peran === '') {
-  console.log('Nama harus di isi!')
-}
-else if (nama !== '' && peran === '') {
-  console.log('Halo ' + nama + ', pilih peranmu untuk memulai game!')
-}
-else if (nama !== '' && peran === 'Ksatria') {
-  console.log('Selamat datang di dunia Proxytia, ' + nama)
-  console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!')
-}
-else if (nama !== '' && peran === 'Tabib') {
-  console.log('Selamat datang di dunia Proxytia, ' + nama)
-  console.log('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka!')
-}
-else if (nama !== '' && peran === 'Penyihir') {
-  console.log('Selamat datang di dunia Proxytia, ' + nama)
-  console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
-}
-else if (nama === '' && (peran === 'Ksatria' || peran === 'Tabib' || peran === 'Penyihir')) {
-  console.log('Nama harus di isi!')
+if (nama !== '' || !nama !== null && nama !== 0) {
+  if (peran === '') {
+    console.log('Halo ' + nama + ', pilih peranmu untuk memulai game!')
+  }
+  else {
+    console.log ('Selamat datang di Dunia Proxytia, ' + nama)
+    if (peran === 'ksatria') {
+      console.log ('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!')
+    }
+    else if (peran === 'tabib') {
+      console.log ('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka!')
+    }
+    else if (peran === 'penyihir') {
+      console.log ('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
+    }
+  }
 }
 else {
-  console.log('Halo ' + nama + ', pilih peran: Ksatria, Tabib atau Penyihir')
+  console.log('Nama harus di isi!')
 }
